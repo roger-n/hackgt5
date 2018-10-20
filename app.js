95db8886f10a068c8fb640671521b76c7e790fd4
Phildesro123 <phildesro@yahoo.com>
2018-10-20 15:13:52 -0400

    Added routes and fixed jquery

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/routes');
 const usersRouter = require('./routes/login');
 
 const app = express();
