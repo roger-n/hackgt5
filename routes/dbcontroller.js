c42d0150a1b0faca4d6693480f5fa3fac0b955f3
Roger <39733923+roger-n@users.noreply.github.com>
2018-10-20 12:38:31 -0400

    idk

--- routes/dbcontroller.js
+++ routes/dbcontroller.js
@@ -8,76 +8,27 @@ const dbconfig = {
     port: 3306
 };
 
-// function getOrders(callback) {
-//     const conn = mysql.createConnection(dbconfig);
-//     conn.connect();
-//     conn.query('SELECT * FROM orders', function (error, results, fields) {
-//         if (error) {
-//             throw error;
-//             conn.end();
-//         }
-//         finalData = results;
-//         if(callback) callback(finalData);
-//         conn.end();
-//     });
-//     // urllib.request(urlToCall, { wd: 'nodejs' }, function (err, data, response) {
-//     //     var statusCode = response.statusCode;
-//     //     finalData = getResponseJson(statusCode, data.toString());
-//     //     // call the function that needs the value
-//     //     callback(finalData);
-//     //     // we are done
-//     //     return;
-//     // });
-// }
 
-function getOrders(fn){
+
+async function getOrders(callback){
     const conn = mysql.createConnection(dbconfig);
     conn.connect();
+    let results = await conn.query('SELECT * FROM orders');
+    console.log(results);
     conn.query('SELECT * FROM orders', function (error, results, fields) {
         if (error) {
-            throw error;
-            conn.end();
+            conn.on('error', function(err) {
+                console.log("[mysql error]",err);
+            });
         }
-        fn(results);
+        console.log(results)
+        //return callback(results);
         conn.end();
     });
-    // geocoder.geocode( { 'address': address}, function(results, status) {
-    //     fn(results);
-    // });
+
 }
 
-getOrders(function(results){
-        return(results); // this is where you get the return value
-});
 
-// function getOrders() {
-//
-//
-//     const conn = mysql.createConnection(dbconfig);
-//
-//     conn.connect();
-//
-//     return new Promise(function(resolve, reject) {
-//             setTimeout(function() {
-//             conn.query('SELECT * FROM orders', function (error, results, fields) {
-//                 if (error) {
-//                     throw error;
-//                     conn.end();
-//                 } else {
-//                     //console.log(results[0]);
-//                     return (results[0], function () {
-//                         conn.end();
-//                     });
-//                     conn.end();
-//                 }
-//
-//             });
-//         }, 2000);
-//     });
-//
-//
-//
-//     conn.end();
-// }
+
 
 module.exports = getOrders;
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,13 @@ const getOrders = require('./dbcontroller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.hbs', { title: 'Express' });
-    var results = getOrders();                              //comment this and the line below it out for testing
-    setTimeout(function(){console.log(results);}, 500);
+    getOrders(function(results){
+        const list = results;
+        console.log(list);
+        res.render('index.hbs', { title: 'Express' });
+
+    });
+
 
 });
 
