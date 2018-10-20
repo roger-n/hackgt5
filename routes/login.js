b48d8194e077c077f063df7048f4943a4c8f8eb6
Roger <39733923+roger-n@users.noreply.github.com>
2018-10-20 01:46:02 -0400

    idk

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('login', { title: 'Express' });
 });
 
 module.exports = router;
