39ed9a8b3970f1164edf94295bb3ef7242b10c67
Phildesro123 <phildesro@yahoo.com>
2018-10-20 11:55:53 -0400

    IDK

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,11 @@ const getOrders = require('./dbcontroller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index.hbs', { title: 'Express' });
-<<<<<<< HEAD
 //    getOrders();
     
-=======
     var results = getOrders();                              //comment this and the line below it out for testing
     setTimeout(function(){console.log(results);}, 500);
 
->>>>>>> d1969089f86677a64b8754d8b4edd3e7f7aeccc5
 });
 
 module.exports = router;
