6f48293eaab4433ed9ded4fcacb0f8947a9e7c0a
Roger <39733923+roger-n@users.noreply.github.com>
2018-10-20 12:55:22 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,12 @@ const getOrders = require('./dbcontroller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-    res.render('index.hbs', { title: 'Express' });
-//    getOrders();
-    
-    var results = getOrders();                              //comment this and the line below it out for testing
-    setTimeout(function(){console.log(results);}, 500);
-=======
+
     getOrders(function(results){
         const list = results;
         console.log(list);
     });
     res.render('index.hbs', { title: 'Express' });
-
->>>>>>> d37aabdbf090bec40676464ede07115cc2e3fdb0
-
 });
 
 module.exports = router;
